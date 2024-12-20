2e31d6c8a78f686293cf3dcb328c677a81c688fc
macdrien <adrien.bouyssou@protonmail.com>
2024-12-20 21:18:42 +0100

    Initial commit from start.spring.io

---
+++ src/main/java/fr/sidranie/newsther/NewstherApplication.java
@@ -0,0 +1,13 @@
+package fr.sidranie.newsther;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewstherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewstherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/sidranie/newsther/NewstherApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sidranie.newsther;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewstherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
