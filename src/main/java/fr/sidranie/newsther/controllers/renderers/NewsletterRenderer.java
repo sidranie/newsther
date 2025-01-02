1f8a220d68d962493b70d8a08f4d7ee536a1be46
macdrien <adrien.bouyssou@protonmail.com>
2025-01-02 14:18:39 +0100

    REF Move templates to newsletters folder

--- src/main/java/fr/sidranie/newsther/controllers/renderers/NewsletterRenderer.java
+++ src/main/java/fr/sidranie/newsther/controllers/renderers/NewsletterRenderer.java
@@ -6,10 +6,13 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import fr.sidranie.newsther.dtos.newsletter.CreateNewsletterDto;
+import fr.sidranie.newsther.dtos.newsletter.FullNewsletterDto;
+import fr.sidranie.newsther.dtos.newsletter.ShortNewsletterDto;
 import fr.sidranie.newsther.entities.Newsletter;
 import fr.sidranie.newsther.mappers.NewsletterMapper;
 import fr.sidranie.newsther.services.NewsletterService;
@@ -26,12 +29,22 @@ public class NewsletterRenderer {
 
     @GetMapping
     public String allNewsletters(Model model) {
-        List<Newsletter> newsletters = service.findAll()
+        List<ShortNewsletterDto> newsletters = service.findAll()
                 .stream()
                 .sorted(Comparator.comparing(Newsletter::getName))
+                .map(NewsletterMapper::newsletterToShortNewsletterDto)
                 .toList();
         model.addAttribute("newsletters", newsletters);
         return "listNewsletters";
+    }
+
+    @GetMapping("/{id}")
+    public String viewNewsletter(@PathVariable("id") Long id, Model model) {
+        FullNewsletterDto fullNewsletterDto = service.findById(id)
+                .map(NewsletterMapper::newsletterToFullNewsletterDto)
+                .orElseThrow(IllegalAccessError::new);
+        model.addAttribute("newsletter", fullNewsletterDto);
+        return "viewNewsletter";
     }
 
     @GetMapping("/create")
@@ -40,7 +53,7 @@ public class NewsletterRenderer {
     }
 
     @PostMapping("/create")
-    public String createNewsletterAction(CreateNewsletterDto createNewsletterDto, Model model) {
+    public String createNewsletterAction(CreateNewsletterDto createNewsletterDto) {
         service.createNewsletter(NewsletterMapper.createNewsletterDtoToNewsletter(createNewsletterDto));
         return "redirect:/newsletters";
     }
