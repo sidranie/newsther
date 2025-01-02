f20e579e158e2a8cd6b18ec155ca8858eb66ec32
macdrien <adrien.bouyssou@protonmail.com>
2025-01-02 13:47:26 +0100

    ADD Create newsletter page

--- src/main/java/fr/sidranie/newsther/controllers/renderers/NewsletterRenderer.java
+++ src/main/java/fr/sidranie/newsther/controllers/renderers/NewsletterRenderer.java
@@ -2,8 +2,12 @@ package fr.sidranie.newsther.controllers.renderers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import fr.sidranie.newsther.dtos.newsletter.CreateNewsletterDto;
+import fr.sidranie.newsther.mappers.NewsletterMapper;
 import fr.sidranie.newsther.services.NewsletterService;
 
 @Controller
@@ -16,9 +20,20 @@ public class NewsletterRenderer {
         this.service = service;
     }
 
-    @RequestMapping
+    @GetMapping
     public String allNewsletters(Model model) {
         model.addAttribute("newsletters", service.findAll());
         return "listNewsletters";
+    }
+
+    @GetMapping("/create")
+    public String createNewsletterForm() {
+        return "createNewsletterForm";
+    }
+
+    @PostMapping("/create")
+    public String createNewsletterAction(CreateNewsletterDto createNewsletterDto, Model model) {
+        service.createNewsletter(NewsletterMapper.createNewsletterDtoToNewsletter(createNewsletterDto));
+        return "redirect:/newsletters";
     }
 }
