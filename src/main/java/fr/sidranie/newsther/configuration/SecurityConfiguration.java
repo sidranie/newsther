a03ce9c9f58622266424e79c63ab617641008ef0
macdrien <adrien.bouyssou@protonmail.com>
2025-01-08 11:26:41 +0100

    ADD H2 console UI access

--- src/main/java/fr/sidranie/newsther/configuration/SecurityConfiguration.java
+++ src/main/java/fr/sidranie/newsther/configuration/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,8 +19,11 @@ public class SecurityConfiguration {
         return http.cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
+                .headers(httpSecurityHeadersConfigurer ->
+                        httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(request ->
                         request.requestMatchers("/api/**").permitAll()
+                                .requestMatchers("/h2-console", "/h2-console/**").permitAll()
                                 .requestMatchers("/login*", "/perform_login", "/people/create").permitAll()
                                 .requestMatchers("/**").authenticated())
                 .formLogin(httpSecurityFormLoginConfigurer ->
