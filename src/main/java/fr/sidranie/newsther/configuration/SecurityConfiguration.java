248512e997521365ee460bc4ca8440f10871a060
macdrien <adrien.bouyssou@gmail.com>
2025-04-18 21:45:11 +0200

    WIP Sample mail sending system

--- src/main/java/fr/sidranie/newsther/configuration/SecurityConfiguration.java
+++ src/main/java/fr/sidranie/newsther/configuration/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration {
                         httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(request ->
                         request.requestMatchers("/").permitAll()
-                                .requestMatchers("/api/**").permitAll()
+                                .requestMatchers("/api/**", "/sendEmail").permitAll()
                                 .requestMatchers("/h2-console", "/h2-console/**").permitAll()
                                 .requestMatchers("/login*", "/perform_login", "/people/create").permitAll()
                                 .requestMatchers("/**").authenticated())
